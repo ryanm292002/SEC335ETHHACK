a4bff97e564628f9d759557cabd4848f28f72db7
ryanm292002 <71150667+ryanm292002@users.noreply.github.com>
2023-02-05 17:54:28 -0500

    Add files via upload

---
+++ week3/dns-resolver.ps1
@@ -0,0 +1,8 @@
+﻿param($network, $server)
+
+$hostid = 0..255
+
+
+foreach ($id in $hostid){
+Resolve-DnsName -DnsOnly "$network.$id" -Server "$server" -ErrorAction Ignore | Select-Object Name," ",NameHost 
+}
