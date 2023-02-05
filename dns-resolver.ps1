beea514c21f80c3230d079935570054cdbad97e5
ryanm292002 <71150667+ryanm292002@users.noreply.github.com>
2023-02-05 17:54:43 -0500

    Delete dns-resolver.ps1

--- dns-resolver.ps1
+++
@@ -1,8 +0,0 @@
-﻿param($network, $server)
-
-$hostid = 0..255
-
-
-foreach ($id in $hostid){
-Resolve-DnsName -DnsOnly "$network.$id" -Server "$server" -ErrorAction Ignore | Select-Object Name," ",NameHost 
-}
